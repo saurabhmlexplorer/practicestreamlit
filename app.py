8576c4583565ec910f58efb5e390c00d486c746f
Saurabh Kumar Yadav <132043474+saurabhmlexplorer@users.noreply.github.com>
2025-03-06 21:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from streamlit_option_menu import option_menu
 st.header("Learning")
 st.write("Creating navigation bar")
 
-'''
+
 # Sidebar menu (Commented out)
 with st.sidebar: 
     selected1 = option_menu(
@@ -18,7 +18,7 @@ if selected1 == "Projects":
     st.title(f"You have selected {selected1}")
 if selected1 == "Contact":
     st.title(f"You have selected {selected1}")
-'''
+
 
 # Horizontal menu
 selected2 = option_menu(
